07ee0b4dcac277bc53fe17e740088cba5adfc78c
Junaid <JSidat95@gmail.com>
2021-03-06 14:03:32 +0000

    post method created in rest controller

--- src/main/java/com/bae/starterproject/repos/BookRepo.java
+++ src/main/java/com/bae/starterproject/repos/BookRepo.java
@@ -1,5 +1,11 @@
 package com.bae.starterproject.repos;
 
-public interface BookRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bae.starterproject.domain.Book;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
 
 }
