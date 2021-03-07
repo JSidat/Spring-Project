6dba749706c4d03f101db073eb4b46b90e23e617
Junaid <JSidat95@gmail.com>
2021-03-07 15:44:15 +0000

    get methods added to controller

--- src/main/java/com/bae/starterproject/rest/BookController.java
+++ src/main/java/com/bae/starterproject/rest/BookController.java
@@ -1,7 +1,11 @@
 package com.bae.starterproject.rest;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,21 @@ public class BookController {
 	@PostMapping("/createBook")
 	public ResponseEntity<Book> createBook(@RequestBody Book book) {
 		return new ResponseEntity<Book>(this.service.createBook(book), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/getBooks")
+	public List<Book> getBooks() {
+		return this.service.getBook();
+	}
+
+	@GetMapping("/getBookById/{id}")
+	public Book getBookById(@PathVariable Long id) {
+		return this.service.getBookById(id);
+	}
+
+	@GetMapping("/getBookByName/{title}")
+	public Book getBookByTitle(@PathVariable String title) {
+		return this.service.getBookByTitle(title);
 	}
 
 }
